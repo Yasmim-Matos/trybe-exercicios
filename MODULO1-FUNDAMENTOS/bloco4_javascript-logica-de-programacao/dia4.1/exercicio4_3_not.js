c9376fddb0c1aba2505394bb1d8510f9c5921d96
Yasmim-Matos <yasmimmn123@gmail.com>
2021-11-09 11:05:18 -0300

    Adicionando exercicio AND e OR e NOT do dia 4.1

---
+++ MODULO1-FUNDAMENTOS/bloco4_javascript-logica-de-programacao/dia4.1/exercicio4_3_not.js
@@ -0,0 +1,31 @@
+//console.log((2 + 2) === 4);
+//O valor retornado do comando acima é true
+
+
+
+
+// console.log(!(2 + 2) === 4);
+//O valor retornado do comando acima é false, pois o operador ! (NOT) tem o poder de inverter o valor booleano de um elemento, seja ele um comando completo ou uma variável, ou strings, números, valores nulos, indefinidos e outros elementos.
+
+
+
+//Strings
+/* const squirtle = "melhor pokemon inicial";
+
+console.log(!squirtle); // false */
+
+
+
+//Números
+/* console.log(!42); // false
+
+console.log(!0); // true */
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+
+//Valores nulos
+//console.log(!null); // true
+
+
+//Valores indefinidos
+console.log(!undefined); // true
