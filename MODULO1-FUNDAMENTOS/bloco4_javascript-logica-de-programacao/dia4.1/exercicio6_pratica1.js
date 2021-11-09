2df05278d27951dfd2313ea2605ed1bf84ab1a9f
Yasmim-Matos <yasmimmn123@gmail.com>
2021-11-09 11:35:27 -0300

    Adicionando parte 1 da prática do dia 4.1

---
+++ MODULO1-FUNDAMENTOS/bloco4_javascript-logica-de-programacao/dia4.1/exercicio6_pratica1.js
@@ -0,0 +1,44 @@
+//1. Fazer 5 programas, um para cada operação aritmética básica
+
+const a = 3;
+const b = 2;
+const c = 4;
+
+/* 
+const adicao = a + b;
+console.log(adicao);
+
+const subtracao = a - b;
+console.log(subtracao);
+
+const multiplicacao = a * b;
+console.log(multiplicacao);
+
+const divisao = a / b;
+console.log(divisao);
+
+const modulo = a % b;
+console.log(modulo);
+ */
+
+//------------------------------------------------------
+
+//2. Faça um programa que retorne o maior de dois números.
+
+/* if (a > b) {
+    console.log("O número", a, "é maior que", b);
+} else {
+    console.log("O número", b, "é maior que", a);
+} */
+
+//------------------------------------------------------
+
+//3. Faça um programa que retorne o maior de três números.
+
+/* if (a > b && a > c){
+    console.log("O número", a, "é maior que", b , "e", c);
+} else if (b > a && b > c){
+    console.log("O número", b, "é maior que", a , "e", c);
+} else {
+    console.log("O número", c, "é maior que", a , "e", b);
+} */
