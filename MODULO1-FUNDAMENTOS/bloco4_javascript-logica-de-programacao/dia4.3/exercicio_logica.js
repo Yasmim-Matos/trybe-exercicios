9ef2570655cf67063b5afc4114785c3d78993c79
Yasmim-Matos <yasmimmn123@gmail.com>
2021-11-20 20:05:21 -0300

    Adicionando exercicio 1 do dia 4.3

---
+++ MODULO1-FUNDAMENTOS/bloco4_javascript-logica-de-programacao/dia4.3/exercicio_logica.js
@@ -0,0 +1,28 @@
+// 1. Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n 
+// Saída:
+/* n = 5
+*****
+*****
+*****
+*****
+***** */
+/* 
+let n = 5;
+let contLinha = '';
+for (let linha = 0; linha < n; linha += 1) {
+    contLinha += '*';
+}
+console.log(contLinha); */
+
+/*
+let n = 5;
+let preencheLinha = '';
+let simbolo = '+';
+
+for (let coluna = 0; coluna < n; coluna += 1) {
+    preencheLinha += simbolo;
+}
+for (let linha = 0; linha < n; linha += 1) {
+    console.log(preencheLinha);
+} */
+
