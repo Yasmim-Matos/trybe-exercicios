ac0b630877ad3dd3b3a88e69cb54f268600baa49
Yasmim-Matos <yasmimmn123@gmail.com>
2022-01-15 20:28:40 -0300

    Adicionando exercicio 1 sobre funcoes do dia 4.4

---
+++ MODULO1-FUNDAMENTOS/bloco4_javascript-logica-de-programacao/dia4.4/Exercícios sobre funções/exercicio1.1_funcoes.js
@@ -0,0 +1,19 @@
+/* 1. Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+Exemplo de palíndromo: arara .
+verificaPalindrome('arara') ;
+Retorno esperado: true
+
+verificaPalindrome('desenvolvimento') ;
+Retorno esperado: false 
+*/
+
+function verificaPalindrome(word) {
+    if (word.split('').reverse().join('') === word) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(verificaPalindrome('arara'));
